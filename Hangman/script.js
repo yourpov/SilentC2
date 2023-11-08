631c09af7bd638b6d3020533bb9fd79a9eff586f
Ecstasy <59181303+ItsEcstasy@users.noreply.github.com>
2023-11-08 16:34:29 -0500

    Add files via upload

---
+++ Hangman/script.js
@@ -0,0 +1,131 @@
+const wordElement = document.getElementById("word");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById(
+  "final-message-reveal-word"
+);
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "application",
+  "programming",
+  "interface",
+  "discord",
+  "telegram",
+  "guilded",
+  "skid",
+  "gay",
+  "botnet",
+  "qbot",
+  "mirai",
+  "rce",
+  "silent",
+  "instagram",
+  "playstation",
+  "function",
+  "return",
+  "length",
+  "type",
+  "node",
+  "arguments",
+  "paramaters",
+  "forloop",
+  "container",
+  "instance",
+  "response",
+  "console",
+  "xbox",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split("") // to array
+      .map(
+        (letter) => `
+    <span class="letter">
+    ${correctLetters.includes(letter) ? letter : ""}
+    </span>
+    `
+      )
+      .join("")} 
+    `; // to string
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations You won😃";
+    finalMessageRevealWord.innerText = "";
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+function UpdateWrongLetters() {
+  wrongLettersElement.innerHTML = `
+  ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+  ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    index < errors
+      ? (part.style.display = "block")
+      : (part.style.display = "none");
+  });
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "You killed the mf😕";
+    finalMessageRevealWord.innerText = `The word was: ${selectedWord}`;
+    popup.style.display = "flex";
+    playable = false;
+  }
+}
+
+function Notify() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+window.addEventListener("keypress", (e) => {
+  if (playable) {
+    const letter = e.key.toLowerCase();
+    if (letter >= "a" && letter <= "z") {
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+          displayWord();
+        } else {
+          Notify();
+        }
+      } else {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+          UpdateWrongLetters();
+        } else {
+          Notify();
+        }
+      }
+    }
+  }
+});
+
+playAgainButton.addEventListener("click", () => {
+  playable = true;
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+  UpdateWrongLetters();
+  popup.style.display = "none";
+});
+
+// Init
+displayWord();
