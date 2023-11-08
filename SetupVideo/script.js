ad47dcd147e5eef0e7c9f7de37e29e859908e5d7
Ecstasy <59181303+ItsEcstasy@users.noreply.github.com>
2023-11-08 18:35:43 -0500

    Add files via upload

---
+++ SetupVideo/script.js
@@ -0,0 +1,58 @@
+// Reference: https://developer.mozilla.org/fr/docs/Learn/JavaScript/Client-side_web_APIs/Video_and_audio_APIs
+
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+const fullScreenButton = document.getElementById("full-screen");
+
+function toggleVideoStatus() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updatePlayIcon() {
+  video.paused
+    ? (play.innerHTML = '<i class="fa fa-play-circle fa-2x"></i>')
+    : (play.innerHTML = '<i class="fa fa-pause-circle fa-2x"></i>');
+}
+
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) minutes = "0" + String(minutes);
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds <10) seconds = "0" + String(seconds);
+  timestamp.innerHTML = `${minutes}:${seconds}`;
+}
+
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+function toggleFullScreen() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
+}
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
+fullScreenButton.addEventListener("click", toggleFullScreen);
