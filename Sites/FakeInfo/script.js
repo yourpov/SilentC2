139de21bcf54887f19bd76ddc6aba57390d7686e
Ecstasy <59181303+ItsEcstasy@users.noreply.github.com>
2023-11-22 20:20:59 -0600

    Update script.js

---
+++ Sites/FakeInfo/script.js
@@ -0,0 +1,172 @@
+// JavaScript code for generating passwords and names
+const passwordResult = document.getElementById("passwordResult");
+const lengthElement = document.getElementById("length");
+const uppercaseElement = document.getElementById("uppercase");
+const lowercaseElement = document.getElementById("lowercase");
+const numbersElement = document.getElementById("numbers");
+const symbolsElement = document.getElementById("symbols");
+const generatePasswordBtn = document.getElementById("generatePassword");
+const passwordClipboard = document.getElementById("passwordClipboard");
+
+const getRandomLower = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+const getRandomUpper = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+const getRandomNumber = () => String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+const getRandomSymbol = () => {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const randomFunctions = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+const createNotification = (message) => {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.innerText = message;
+  document.body.appendChild(notif);
+  setTimeout(() => notif.remove(), 3000);
+};
+
+passwordClipboard.addEventListener("click", () => {
+  const password = passwordResult.innerText;
+  if (!password) return;
+  const textarea = document.createElement("textarea");
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  createNotification("Password copied to clipboard!");
+});
+
+generatePasswordBtn.addEventListener("click", () => {
+  const length = +lengthElement.value;
+  const hasLower = lowercaseElement.checked;
+  const hasUpper = uppercaseElement.checked;
+  const hasNumber = numbersElement.checked;
+  const hasSymbol = symbolsElement.checked;
+  passwordResult.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+const generatePassword = (lower, upper, number, symbol, length) => {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) return "";
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunctions[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+};
+
+const nameResult = document.getElementById("nameResult");
+const generateNameBtn = document.getElementById("generateName");
+const nameClipboard = document.getElementById("nameClipboard");
+
+const firstNames = [
+  "James",
+  "John",
+  "Robert",
+  "Michael",
+  "David",
+  "William",
+  "Richard",
+  "Joseph",
+  "Charles",
+  "Thomas",
+  "Daniel",
+  "Matthew",
+  "Anthony",
+  "Donald",
+  "Mark",
+  "Paul",
+  "Steven",
+  "Andrew",
+  "Kenneth",
+  "Joshua",
+  "Kevin",
+  "Brian",
+  "George",
+  "Edward",
+  "Ronald",
+  "Timothy",
+  "Jason",
+  "Jeffrey",
+  "Ryan",
+  "Jacob",
+  "Gary",
+  "Nicholas",
+];
+
+const lastNames = [
+  "Smith",
+  "Johnson",
+  "Williams",
+  "Brown",
+  "Jones",
+  "Garcia",
+  "Miller",
+  "Davis",
+  "Rodriguez",
+  "Martinez",
+  "Hernandez",
+  "Lopez",
+  "Gonzalez",
+  "Wilson",
+  "Anderson",
+  "Thomas",
+  "Taylor",
+  "Moore",
+  "Jackson",
+  "Martin",
+  "Lee",
+  "Perez",
+  "Thompson",
+  "White",
+  "Harris",
+  "Sanchez",
+  "Clark",
+  "Ramirez",
+  "Lewis",
+  "Robinson",
+  "Wells",
+];
+
+const getRandomName = () => {
+    const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    return `${firstName} ${lastName}`;
+  };
+  
+  nameClipboard.addEventListener("click", () => {
+    const fullName = nameResult.innerText;
+    if (!fullName) return;
+    const textarea = document.createElement("textarea");
+    textarea.value = fullName;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    createNotification("Name copied to clipboard!");
+  });
+  
+  generateNameBtn.addEventListener("click", () => {
+    const name = getRandomName();
+    nameResult.innerText = name;
+  });
